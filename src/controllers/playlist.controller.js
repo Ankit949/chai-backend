359d531f6ec575e7b8ca302360685da4fbacb215
Ankit949 <akgiri949@gmail.com>
2024-07-07 22:46:10 +0530

    added playlist controller

--- src/controllers/playlist.controller.js
+++ src/controllers/playlist.controller.js
@@ -1,5 +1,6 @@
 import mongoose, {isValidObjectId} from "mongoose"
 import {Playlist} from "../models/playlist.model.js"
+import {Video} from "../models/video.model.js"
 import {ApiError} from "../utils/ApiError.js"
 import {ApiResponse} from "../utils/ApiResponse.js"
 import {asyncHandler} from "../utils/asyncHandler.js"
@@ -58,23 +59,119 @@ const getPlaylistById = asyncHandler(async (req, res) => {
 
 const addVideoToPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+
+    if (!playlistId || playlistId.trim() === "" ) throw new ApiError(400, "playlistId is a required argument")
+
+    if (!videoId || videoId.trim() === "") throw new ApiError(400, "videoId is a required argument")
+
+    if(!isValidObjectId(playlistId)) throw new ApiError(400, "Invalid Playlist Id")
+
+    if(!isValidObjectId(videoId)) throw new ApiError(400, "Invalid video Id")
+    
+    const fetchPlaylist = await Playlist.findById(playlistId)
+    if(!fetchPlaylist) throw new ApiError(400, "No Playlist found")
+
+    const videoDetails = await Video.findById(videoId)
+    if(!videoDetails) throw new ApiError(400, "No data found")
+
+    const addPlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $push:{
+                videos: videoId
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!addPlaylist) throw new ApiError(400, "Error while adding video to playlist")
+
+    res.status(200)
+    .json(new ApiResponse(200, addPlaylist, "Video added successfully"))
     
 })
 
 const removeVideoFromPlaylist = asyncHandler(async (req, res) => {
     const {playlistId, videoId} = req.params
+
+    if (!playlistId || playlistId.trim() === "" ) throw new ApiError(400, "playlistId is a required argument")
+
+    if (!videoId || videoId.trim() === "") throw new ApiError(400, "videoId is a required argument")
+
+    if(!isValidObjectId(playlistId)) throw new ApiError(400, "Invalid Playlist Id")
+
+    if(!isValidObjectId(videoId)) throw new ApiError(400, "Invalid video Id")
     
+    const fetchPlaylist = await Playlist.findById(playlistId)
+    if(!fetchPlaylist) throw new ApiError(400, "No Playlist found")
+
+    const videoDetails = await Video.findById(videoId)
+    if(!videoDetails) throw new ApiError(400, "No data found")
+
+    const addPlaylist = await Playlist.findByIdAndUpdate(
+        playlistId,
+        {
+            $pull:{
+                videos: videoId
+            }
+        },
+        {
+            new: true
+        }
+    )
+
+    if(!addPlaylist) throw new ApiError(400, "Error while adding video to playlist")
+
+    res.status(200)
+    .json(new ApiResponse(200, addPlaylist, "Video added successfully"))
 
 })
 
 const deletePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
+    if (!playlistId || playlistId.trim() === "" ) throw new ApiError(400, "playlistId is a required argument")
     
+    if(!isValidObjectId(playlistId)) throw new ApiError(400, "Invalid Playlist Id")
+
+    const deleteResponse = await Playlist.findByIdAndDelete(
+        playlistId
+    )
+
+    if(!deleteResponse) throw new ApiError(400, "Error while deleting playlist")
+    
+    res.status(200)
+    .json(new ApiResponse(200, deleteResponse, "Playlist deleted response"))
 })
 
 const updatePlaylist = asyncHandler(async (req, res) => {
     const {playlistId} = req.params
     const {name, description} = req.body
+
+    if (!playlistId || playlistId.trim() === "" ) throw new ApiError(400, "playlistId is a required argument")
+    
+    if(!isValidObjectId(playlistId)) throw new ApiError(400, "Invalid Playlist Id")
+
+    if(name){
+        if(name.trim() == "") throw new ApiError(400, "Name can't be empty")
+    }
+    const playlistDetails = await Video.findByIdAndUpdate(playlistId,
+            {
+                $set: {
+                  ...(name && { name: name.trim()}),
+                  ...(description && { description: description.trim()})
+                }
+            },
+            {
+                new: true
+            }
+    )
+
+    if(!playlistDetails) throw new ApiError(400, "Something went wrong while updating playlist")
+
+    res.status(200)
+    .json(new ApiResponse(200, playlistDetails, "Playlist updated successfully"))
     
 })
 
